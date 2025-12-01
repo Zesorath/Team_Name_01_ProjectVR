f9cf02f3f752f4b0edb58bbc65844d04f7dce7fa
MEuph0 <mason.willy0923@gmail.com>
2025-12-01 04:15:29 -0600

    Finished grabbable tablet UI and text pages
    Still need to implement Lesson Plans into tablet text and possibly customize for each level but this is a functioning prototype of what will likely be used for the project.

--- Assets/Scripts/TabletButton.cs
+++ Assets/Scripts/TabletButton.cs
@@ -1,56 +1,66 @@
+using System.Runtime.CompilerServices;
 using NUnit.Framework;
-using System.Collections;
-using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
+using TMPro;
+using UnityEngine.Rendering;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class TabletContents : MonoBehaviour
 {
-    [SerializeField] List<Transform> pages;
-    int index = -1;
-    [SerializeField] GameObject BackButton;
-    [SerializeField] GameObject NextButton;
+    [TextArea(10, 20)]
+    [SerializeField] private string content;
+    [Space]
+    [SerializeField] private TMP_Text display;
+    [Space]
+    [SerializeField] private TMP_Text textPagination;
 
-    private void Start()
+    private void OnValidate()
     {
-        BackButton.SetActive(false);
+        UpdatePagination();
+
+        if (display.text == content)
+            return;
+
+        SetupContent();
+    }
+
+    private void Awake()
+    {
+        SetupContent();
+        UpdatePagination();
+    }
+
+    private void SetupContent()
+    {
+        display.text = content;
+    }
+
+    private void UpdatePagination()
+    {
+        textPagination.text = display.pageToDisplay.ToString();
+    }
+
+    public void PrevPage()
+    {
+        if (display.pageToDisplay <= 1)
+        {
+            display.pageToDisplay = 1;
+            return;
+        }
+
+        display.pageToDisplay -= 1;
+
+        UpdatePagination();
     }
 
     public void NextPage()
     {
-        index++;
-        ForwardButtonActions();
-        pages[index].SetAsLastSibling();
-    }
+        if (display.pageToDisplay >= display.textInfo.pageCount)
+            return;
 
-    public void ForwardButtonActions()
-    {
-        if (BackButton.activeInHierarchy == false)
-        {
-            BackButton.SetActive(false);
-        }
-        if (index == pages.Count - 1)
-        {
-            NextButton.SetActive(false);
-        }
-    }
+        display.pageToDisplay += 1;
 
-    public void BackPage()
-    {
-        pages[index].SetAsLastSibling();
-        BackButtonActions();
-
-    }
-
-    public void BackButtonActions()
-    {
-        if(NextButton.activeInHierarchy == false)
-        {
-            NextButton.SetActive(true);
-        }
-        if(index - 1 == -1)
-        {
-            BackButton.SetActive(false);
-        }
+        UpdatePagination();
     }
 
     
