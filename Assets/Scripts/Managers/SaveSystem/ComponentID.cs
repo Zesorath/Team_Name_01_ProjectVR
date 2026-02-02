abbf80adb2e27e655217b3e28903c796eb9116d9
Mo <MoTaylorWilliams@gmail.com>
2026-02-02 01:28:50 -0600

    Fixed multiple-registration bug in Load()
    Made components from spawners wait until first release to register with the save manager, so spawner display objects don't litter the save file

--- Assets/Scripts/Managers/SaveSystem/ComponentID.cs
+++ Assets/Scripts/Managers/SaveSystem/ComponentID.cs
@@ -1,21 +1,24 @@
 using System;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class ComponentID : MonoBehaviour
 {
+    // ID FIELDS AND FUNCTIONS
     public Guid id;
     public string label = "";
     public ComponentTypes.Types type = ComponentTypes.Types.DEFAULT;
     public int index = -1;
 
-    // Runs on object spawn. Generates a unique ID if one doesn't already exist,
-    // and calls GenerateLabelSuggestion() to populate the label field
-    public void Awake()
+    // Acts as a constructor. Generates a unique ID if one doesn't already 
+    // exist, and calls GenerateLabelSuggestion() to populate the label field
+    void Init()
     {
-        // id will already exist on load
+        // id will already exist if building from a save file
         if (id == Guid.Empty) id = Guid.NewGuid();
-        
+
         // Error states--abort initialization
         string errMsg = "[ComponentID]: Initialization failure--";
         if (SaveManager.Instance == null)
@@ -27,14 +30,13 @@ public class ComponentID : MonoBehaviour
         // Initialization succeeded--register component
         Debug.Log($"[ComponentID]: INITIALIZED--ID = {id}, label = {label}");
         SaveManager.Instance.Register(this);
+        registered = true;
     }
-
-    public void OnDestroy() { Debug.Log($"[ComponentID]: DESTROYED {id}"); }
 
     // Generates a label from the component type and the next available index 
     // of that type. Type is set by the component prefab. Populates index field
     // from the type's current count.
-    public void GenerateLabelSuggestion()
+    void GenerateLabelSuggestion()
     {
         string errMsg = "[ComponentID]: Label generation failure--";
         
@@ -55,4 +57,67 @@ public class ComponentID : MonoBehaviour
 
     // Revert back to the automatically suggested label
     public void RevertLabel() { label = $"{type}_{index}"; }
+
+
+    // LIFETIME FIELDS AND FUNCTIONS
+
+    // Used for registering on first release, to avoid a bug in Load()
+    bool registered = false;
+    XRGrabInteractable grab;
+    // Spawner to avoid
+    ItemSpawner os; // Origin spawner
+    Transform osTransform;
+    float osRadius;
+
+    // Runs on object spawn.
+    public void Awake()
+    {        
+        // Grab a reference to the XR component
+        grab = GetComponent<XRGrabInteractable>();
+        if (grab == null) 
+        {
+            string msg = $"{gameObject.name} missing XRGrabInteractable";
+            Debug.Log($"[ComponentID]: {msg}");
+            return;
+        }
+
+        // There will be no ItemSpawner if the object is spawned from save file,
+        // so go ahead and register the Component
+        os = GetComponentInParent<ItemSpawner>();
+        if (os == null) { Init(); return; }
+
+        // Otherwise, cache origin spawner's position, rotation, and size
+        osTransform = os.transform;
+        osRadius = os.Respawn_Radius;
+    }
+
+    // Define grab listener
+    void OnReleased(SelectExitEventArgs args)
+    {
+        // Skip if already registered
+        if (registered) return;
+
+        // Register first time object is released outside the spawner radius, 
+        // plus a tiny epsilon to prevent jitter
+        float d = Vector3.Distance(transform.position, osTransform.position);
+        if (d > osRadius + 0.01f) Init();
+    }
+
+    // Subscribe to grab listener on Awake()
+    void OnEnable()
+    {
+        if (grab != null) grab.selectExited.AddListener(OnReleased);
+    }
+
+    // Un-subscribe to grab listener when the object is destroyed
+    void OnDisable()
+    {
+        if (grab != null) grab.selectExited.RemoveListener(OnReleased);
+    }
+
+    // Move id/label generation and registration (basically everything in old Awake()) to OnRelease()
+
+
+    // Just announce when the ComponentID object is destroyed
+    public void OnDestroy() { Debug.Log($"[ComponentID]: DESTROYED {id}"); }
 }
