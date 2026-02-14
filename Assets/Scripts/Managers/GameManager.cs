48585d887f9696625870c7d04bbdf4ab9f82a969
Zesorath <jakob.smith0201@gmail.com>
2026-02-14 12:40:11 -0600

    Fixing Reset and Lesson Loader
    Fixed the Reset Scene button by creating the actual code to reset the current scene rather than load a different one for testing purposes.  Uses the SceneManager to find the current one and just use that to reload.
    Lessons now are on unity (tutorial and lessons 1-6 as separate scenes with a different text for easy understanding of which one they are on) and the code works to switch between them.

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,13 +4,40 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     //[SerializeField] private string openLessonName;
-    public void OpenStartScene()
+
+    public void ReloadCurrentScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void Tutorial()
     {
         SceneManager.LoadScene(1);
     }
-    public void OpenWorkshop()
+   
+
+    public void Lesson_1()
     {
         SceneManager.LoadScene(2);
     }
+    public void Lesson_2()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void Lesson_3()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void Lesson_4()
+    {
+        SceneManager.LoadScene(5);
+    }
 
+    public void Lesson_5()
+    {
+        SceneManager.LoadScene(6);
+    }
+    public void Lesson_6()
+    {
+        SceneManager.LoadScene(7);
+    }
 }
