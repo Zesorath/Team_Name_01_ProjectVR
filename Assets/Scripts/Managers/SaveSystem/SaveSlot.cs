219b12a313fc98aad34210626254d8e0e8d0d193
Mo <MoTaylorWilliams@gmail.com>
2026-03-09 20:35:51 -0500

    SaveSlot accessors and mutators

--- Assets/Scripts/Managers/SaveSystem/SaveSlot.cs
+++ Assets/Scripts/Managers/SaveSystem/SaveSlot.cs
@@ -1,8 +1,40 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SaveSlot
 {
-    public int id;
-    public string fileName = "";
-    public string scene = "Sandbox";
+    [NonSerialized]
+    readonly SaveDebug d = new SaveDebug("<color=#5C6BC0>[SaveSlot] </color>");
+    
+    public int slotID;
+    string display = "[EMPTY]";
+    string fileName = "";
+    string level = "";
+    DateTime whenLastUsed = DateTime.MinValue;
+
+    public void CaptureSlotMetadata(SaveData sd)
+    {
+        fileName = $"{sd.saveID}.json";
+        level = SceneManager.GetActiveScene().name;
+        whenLastUsed = DateTime.UtcNow;
+    }
+
+    public void CaptureWhenLastUsed() { whenLastUsed = DateTime.UtcNow; }
+
+    // Returns UTC timestamp object
+    public DateTime GetWhenLastUsed_UTC() { return whenLastUsed; }
+    
+    // Returns local timestamp object
+    public DateTime GetWhenLastUsed_local() 
+        { return whenLastUsed.ToLocalTime(); }
+    
+    // Returns local timestamp as a formatted string
+    public string GetWhenLastUsed_formatted()
+    {
+        // Show nothing if there is no timestamp yet
+        if (whenLastUsed == DateTime.MinValue) return "";
+        
+        return whenLastUsed.ToLocalTime().ToString("yyyy-MM-dd HH:mm");
+    }
 }
