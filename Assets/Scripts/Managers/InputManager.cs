8b0623bd06f76d6fd0095be071f45e3a7dd531fc
Mo <MoTaylorWilliams@gmail.com>
2026-03-10 04:34:16 -0500

    Made persistent and connected some save slots to keyboard buttons for debugging

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -5,6 +5,20 @@ using UnityEngine;
 /// </summary>
 public class InputManager : MonoBehaviour
 {
+    private static InputManager instance;
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    
     /// <summary>
     /// Register all hotkeys
     /// </summary>
@@ -26,5 +40,13 @@ public class InputManager : MonoBehaviour
             Debug.Log($"{splash}F9 pressed. Calling QuickLoad()");
             sm.QuickLoad();
         }
+
+        if (Input.GetKeyDown(KeyCode.F6)) sm.SaveToSlot(1);
+        if (Input.GetKeyDown(KeyCode.F7)) sm.SaveToSlot(2);
+        if (Input.GetKeyDown(KeyCode.F8)) sm.SaveToSlot(3);
+
+        if (Input.GetKeyDown(KeyCode.F10)) sm.LoadFromSlot(1);
+        if (Input.GetKeyDown(KeyCode.F11)) sm.LoadFromSlot(2);
+        if (Input.GetKeyDown(KeyCode.F12)) sm.LoadFromSlot(3);
     }
 }
