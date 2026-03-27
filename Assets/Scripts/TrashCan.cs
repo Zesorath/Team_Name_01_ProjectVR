99d3adb5bfb106a2b7867a427abd6d244c96790a
Christian Smith <christiansmith4@my.unt.edu>
2026-03-26 19:48:39 -0500

    trashcan and grabbing fixes

--- Assets/Scripts/TrashCan.cs
+++ Assets/Scripts/TrashCan.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class TrashCan : MonoBehaviour
@@ -7,6 +8,17 @@ public class TrashCan : MonoBehaviour
         ComponentID cID = other.GetComponent<ComponentID>();
         if (cID)
         {
+            if(other.gameObject.CompareTag("Wire End"))
+            {
+                GameObject obj = other.gameObject;
+                GameObject wireParent = obj.GetComponent<WireEnd>().parentWire.gameObject;
+                cID = wireParent.GetComponent<ComponentID>();
+                print("!!!wire " + wireParent + ", " + cID);
+            }
+            else
+            {
+                print("!!!not wire");    
+            }
             cID.Delete();
         }
     }
